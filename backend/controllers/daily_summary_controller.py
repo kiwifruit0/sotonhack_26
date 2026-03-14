2bc06feb59d3cf765f8f4166f8dd50c083c0cc85
Milan <mjg1g24@soton.ac.uk>
2026-03-14 23:55:01 +0000

    audio

--- backend/controllers/daily_summary_controller.py
+++ backend/controllers/daily_summary_controller.py
@@ -1,12 +1,24 @@
 from pydub import AudioSegment
+import requests
 from .speech_controller import output_speech
+from ..routers.db_router import get_daily_summary
 import io
+import asyncio
 
 short_pause = AudioSegment.silent(duration=500)
 long_pause = AudioSegment.silent(duration=1500)
+def get_audio_from_url(url, format):
+    response = requests.get(url, )
+    response.raise_for_status() #
+    
+    audio_data = io.BytesIO(response.content)
+    
+    audio = AudioSegment.from_file(audio_data)
+    
+    return audio
 
 async def collate_summaries(user):
-    summaries = {}
+    summaries = get_daily_summary(user)["friendSummaries"]
     combined_audio = AudioSegment.empty()
 
     intro_generator = output_speech(None, "Here's what your friends are up to")
