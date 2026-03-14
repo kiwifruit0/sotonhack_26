974501dd62140296cabe5a2649653d3633687e38
Milan <mjg1g24@soton.ac.uk>
2026-03-14 23:05:09 +0000

    summary audio

--- backend/controllers/daily_summary_controller.py
+++ backend/controllers/daily_summary_controller.py
@@ -7,16 +7,15 @@ short_pause = AudioSegment.silent(duration=500)
 long_pause = AudioSegment.silent(duration=1500)
 
 async def collate_summaries(user):
-    friends = None
-    summaries = {}
+    summaries = ""
     combined_audio = AudioSegment.empty()
-    
-
+    firstAudio = AudioSegment.from_file(output_speech(None, "Here's what your friends are up to"))
+    combined_audio += firstAudio
     for name, audio in summaries.items():
         try:
             intro_file_path = output_speech(None, f"{name} says")
             intro_seg = AudioSegment.from_file(intro_file_path)
-            summary_seg = AudioSegment.from_file(audio)
+            summary_seg = AudioSegment.from_(audio)
             combined_audio += intro_seg + short_pause + summary_seg + long_pause
         except Exception as e:
                 print(f"Error processing {name}: {e}")
